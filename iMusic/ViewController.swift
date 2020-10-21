74934ad4dba98946085dec854c339fde84b9b2eb
Arkadii Zuev <arkasha.zuev@icloud.com>
2020-10-21 18:26:48 +0500

    Initial Commit

---
+++ iMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Arkasha Zuev on 21.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
