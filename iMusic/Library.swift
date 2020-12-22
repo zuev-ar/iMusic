73cd82993ab80d4335ffb64536e9a593123b96b5
Arkadii Zuev <arkasha.zuev@icloud.com>
2020-12-22 19:51:16 +0500

    SwiftUI gestures and custom buttons

--- iMusic/Library.swift
+++ iMusic/Library.swift
@@ -10,7 +10,11 @@ import URLImage
 
 struct Library: View {
     
-    var tracks = UserDefaults.standard.savedTracks()
+    @State var tracks = UserDefaults.standard.savedTracks()
+    @State private var showingAlert = false
+    @State private var track: SearchViewModel.Cell!
+    
+    var tabBarDelegate: MainTabBarControllerDelegate?
     
     var body: some View {
         NavigationView {
@@ -18,7 +22,8 @@ struct Library: View {
                 GeometryReader { geometry in
                     HStack (spacing: 20) {
                         Button(action: {
-                            
+                            self.track = self.tracks[0]
+                            self.tabBarDelegate?.maximizeTrackDetailController(viewModel: self.track)
                         }, label: {
                             Image(systemName: "play.fill")
                                 .frame(width: geometry.size.width / 2 - 10, height: 50)
@@ -27,7 +32,7 @@ struct Library: View {
                                 .cornerRadius(10)
                         })
                         Button(action: {
-                            
+                            self.tracks = UserDefaults.standard.savedTracks()
                         }, label: {
                             Image(systemName: "arrow.2.circlepath")
                                 .frame(width: geometry.size.width / 2 - 10, height: 50)
@@ -39,12 +44,48 @@ struct Library: View {
                 }.padding().frame(height: 50)
                 Divider().padding(.leading).padding(.trailing).padding(.top)
                 
-                List (tracks) { track in
-                    LibraryCell(cell: track)
+                List {
+                    ForEach(tracks) { track in
+                        LibraryCell(cell: track).gesture(
+                            LongPressGesture()
+                                .onEnded { _ in
+                                    self.track = track
+                                    self.showingAlert = true
+                                }
+                                .simultaneously(with: TapGesture()
+                                    .onEnded { _ in
+                                        self.track = track
+                                        self.tabBarDelegate?.maximizeTrackDetailController(viewModel: track)
+                                    }))
+                    }
+                    .onDelete(perform: delete)
                 }
             }
+            .actionSheet(isPresented: $showingAlert, content: {
+                ActionSheet(title: Text("Are you sure you want to delete this track?"), buttons: [.destructive(Text("Delete"), action: {
+                    self.delete(track: self.track)
+                }), .cancel()])
+            })
         }
         .navigationTitle("Library")
+    }
+    
+    func delete(at offsets: IndexSet) {
+        tracks.remove(atOffsets: offsets)
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: tracks, requiringSecureCoding: false) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: UserDefaults.favouriteTrackKey)
+        }
+    }
+    
+    func delete(track: SearchViewModel.Cell) {
+        let index = tracks.firstIndex(of: track)
+        guard let myIndex = index else { return }
+        tracks.remove(at: myIndex)
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: tracks, requiringSecureCoding: false) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: UserDefaults.favouriteTrackKey)
+        }
     }
     
 }
--- iMusic/MainTabBarController.swift
+++ iMusic/MainTabBarController.swift
@@ -31,7 +31,9 @@ class MainTabBarController: UITabBarController {
         
         searchVC.tabBarDelegate = self
         
-        let library = Library()
+        var library = Library()
+        library.tabBarDelegate = self
+        
         let hostVC = UIHostingController(rootView: library)
         hostVC.tabBarItem.image = #imageLiteral(resourceName: "library")
         hostVC.tabBarItem.title = "Library"
